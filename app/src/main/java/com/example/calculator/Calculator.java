b2971f60b6487709ac5b84b33d2d0ffab55b4322
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2019-02-07 12:57:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package com.example.calculator;
+
+
+
+public class Calculator {
+
+    public static int add(int a, int b){
+        int result = a + b;
+        return result;
+    }
+
+    public int subtraction(int a, int b){
+        int result = a - b;
+        return result;
+    }
+
+    public int multiplication(int a, int b){
+        int result = a * b;
+        return result;
+    }
+
+    public int division(int a, int b){
+        int result = a / b;
+        return result;
+    }
+}
